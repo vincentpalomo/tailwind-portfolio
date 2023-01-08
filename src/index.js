4c63f976b27ee272ebfd5fb6fc1ac34de3788e67
Vincent Palomo <vincentpalomo@yahoo.com>
2023-01-07 19:42:38 -0600

    set up layout of website

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import { createRoot } from 'react-dom/client';
 import style from './global.css';
 
 const App = () => {
-  return <div className='text-3xl'>Hello World</div>;
+  return (
+    <div className='flex flex-col justify-between h-screen'>
+      {/* Header */}
+      <header className='bg-slate-100'>nav</header>
+      {/* Body */}
+      <section className='flex-grow mb-auto overflow-y-auto'>body</section>
+      {/* Footer */}
+      <footer className='bg-slate-100'>footer</footer>
+    </div>
+  );
 };
 
 const container = document.getElementById('app');
