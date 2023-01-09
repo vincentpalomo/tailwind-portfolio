f0dd5542a5ec4b1018dcffe450036f500bad65ec
Vincent Palomo <vincentpalomo@yahoo.com>
2023-01-09 16:25:11 -0600

    changed text in the header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,65 +1,54 @@
 import React from 'react';
 
-export default function Navbar({ fixed }) {
-  const [navbarOpen, setNavbarOpen] = React.useState(false);
+const Navbar = () => {
   return (
-    <>
-      <nav className='relative flex flex-wrap items-center justify-between px-2 py-3 bg-gray-900 mb-3'>
-        <div className='container px-4 mx-auto flex flex-wrap items-center justify-between'>
-          <div className='w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start'>
-            <a
-              className='text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white'
-              href='#pablo'
-            >
-              Vincent Palomo
-            </a>
-            <button
-              className='text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none'
-              type='button'
-              onClick={() => setNavbarOpen(!navbarOpen)}
-            >
-              <i className='fas fa-bars'></i>
-            </button>
-          </div>
-          <div
-            className={
-              'lg:flex flex-grow items-center' +
-              (navbarOpen ? ' flex' : ' hidden')
-            }
-            id='example-navbar-danger'
+    <nav
+      aria-label='Site Nav'
+      class='mx-auto flex max-w-3xl items-center justify-between p-4'
+    >
+      <span>Vincent Palomo 👋</span>
+
+      <ul class='flex items-center gap-2 text-sm font-medium text-gray-500'>
+        <li class='hidden lg:block'>
+          <a class='rounded-lg px-3 py-2' href='/'>
+            {' '}
+            Home{' '}
+          </a>
+        </li>
+
+        <li>
+          <a class='rounded-lg px-3 py-2' href=''>
+            {' '}
+            Projects{' '}
+          </a>
+        </li>
+
+        <li>
+          <a
+            class='inline-flex items-center rounded-lg px-3 py-2'
+            href=''
+            target='_blank'
           >
-            <ul className='flex flex-col lg:flex-row list-none lg:ml-auto'>
-              <li className='nav-item'>
-                <a
-                  className='px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75'
-                  href='#pablo'
-                >
-                  <i className='fab fa-facebook-square text-lg leading-lg text-white opacity-75'></i>
-                  <span className='ml-2'>My Work</span>
-                </a>
-              </li>
-              <li className='nav-item'>
-                <a
-                  className='px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75'
-                  href='#pablo'
-                >
-                  <i className='fab fa-twitter text-lg leading-lg text-white opacity-75'></i>
-                  <span className='ml-2'>Github</span>
-                </a>
-              </li>
-              <li className='nav-item'>
-                <a
-                  className='px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75'
-                  href='#pablo'
-                >
-                  <i className='fab fa-pinterest text-lg leading-lg text-white opacity-75'></i>
-                  <span className='ml-2'>Hire Me</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </>
+            External
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+              class='ml-1.5 h-4 w-4'
+            >
+              <path
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                stroke-width='2'
+                d='M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14'
+              ></path>
+            </svg>
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
-}
+};
+
+export default Navbar;
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -5,7 +5,9 @@ const Timeline = () => {
     <section class='bg-gray-900 text-white'>
       <div class='max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8'>
         <div class='max-w-xl'>
-          <h2 class='text-3xl font-bold sm:text-4xl'>What makes us special</h2>
+          <h2 class='text-3xl font-bold sm:text-4xl'>
+            Here is some of my work ✌️
+          </h2>
 
           <p class='mt-4 text-gray-300'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
@@ -36,7 +38,36 @@ const Timeline = () => {
             </span>
 
             <div class='ml-4'>
-              <h2 class='text-lg font-bold'>Lorem, ipsum dolor.</h2>
+              <h2 class='text-lg font-bold'>Strangers Things</h2>
+
+              <p class='mt-1 text-sm text-gray-300'>
+                React App mockup of craiglist. Utilizing API calls (CRUD).
+              </p>
+            </div>
+          </div>
+
+          <div class='flex items-start'>
+            <span class='flex-shrink-0 rounded-lg bg-gray-800 p-4'>
+              <svg
+                class='h-5 w-5'
+                fill='none'
+                stroke='currentColor'
+                viewBox='0 0 24 24'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path d='M12 14l9-5-9-5-9 5 9 5z'></path>
+                <path d='M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z'></path>
+                <path
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                  stroke-width='2'
+                  d='M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222'
+                ></path>
+              </svg>
+            </span>
+
+            <div class='ml-4'>
+              <h2 class='text-lg font-bold'>Valorant React App</h2>
 
               <p class='mt-1 text-sm text-gray-300'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
@@ -67,7 +98,7 @@ const Timeline = () => {
             </span>
 
             <div class='ml-4'>
-              <h2 class='text-lg font-bold'>Lorem, ipsum dolor.</h2>
+              <h2 class='text-lg font-bold'>Caesar Cipher React</h2>
 
               <p class='mt-1 text-sm text-gray-300'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
@@ -98,7 +129,7 @@ const Timeline = () => {
             </span>
 
             <div class='ml-4'>
-              <h2 class='text-lg font-bold'>Lorem, ipsum dolor.</h2>
+              <h2 class='text-lg font-bold'>Dreamland API</h2>
 
               <p class='mt-1 text-sm text-gray-300'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
@@ -129,7 +160,7 @@ const Timeline = () => {
             </span>
 
             <div class='ml-4'>
-              <h2 class='text-lg font-bold'>Lorem, ipsum dolor.</h2>
+              <h2 class='text-lg font-bold'>Tic-Tac-Toe React</h2>
 
               <p class='mt-1 text-sm text-gray-300'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
@@ -160,38 +191,7 @@ const Timeline = () => {
             </span>
 
             <div class='ml-4'>
-              <h2 class='text-lg font-bold'>Lorem, ipsum dolor.</h2>
-
-              <p class='mt-1 text-sm text-gray-300'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                cumque tempore est ab possimus quisquam reiciendis tempora
-                animi! Quaerat, saepe?
-              </p>
-            </div>
-          </div>
-
-          <div class='flex items-start'>
-            <span class='flex-shrink-0 rounded-lg bg-gray-800 p-4'>
-              <svg
-                class='h-5 w-5'
-                fill='none'
-                stroke='currentColor'
-                viewBox='0 0 24 24'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <path d='M12 14l9-5-9-5-9 5 9 5z'></path>
-                <path d='M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z'></path>
-                <path
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
-                  stroke-width='2'
-                  d='M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222'
-                ></path>
-              </svg>
-            </span>
-
-            <div class='ml-4'>
-              <h2 class='text-lg font-bold'>Lorem, ipsum dolor.</h2>
+              <h2 class='text-lg font-bold'>Pokedex React</h2>
 
               <p class='mt-1 text-sm text-gray-300'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
