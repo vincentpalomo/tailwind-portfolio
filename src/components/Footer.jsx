572ac4e083e4532ce3d21c1c79255b389c492da8
vincent <vincentpalomo@yahoo.com>
2023-01-24 18:22:39 -0600

    removed all code in components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
-<<<<<<< HEAD
-  return <div>hello from footer</div>;
-=======
-  return (
-    <div class='w-full p-4 bg-gray-900 text-gray-300'>
-      <div class='text-center'>
-        <p>
-          Copyright © 2022 -
-          <a class='font-semibold' href='mailto:vincentpalomo@yahoo.com'>
-            Vincent Palomo
-          </a>
-        </p>
-      </div>
-    </div>
-  );
->>>>>>> 3f03ba1c85d6ac3f2a03243d8286354dac1d7cde
+  return <div>Footer</div>;
 };
 
 export default Footer;
