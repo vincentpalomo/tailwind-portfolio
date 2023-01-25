52c491bb43a25f0757d68b65c4be0055d3ce0431
vincent <vincentpalomo@yahoo.com>
2023-01-24 19:48:29 -0600

    remade the navbar and fixed the body of the website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className='bg-gray-800'>
+      <div className='container px-6 py-4 mx-auto'>
+        <div className='lg:flex lg:items-center'>
+          <div className='flex items-center justify-between'>
+            <div>
+              <a href='#' className='text-white'>
+                👋 Vincent Palomo
+              </a>
+            </div>
+            <div className=''>
+              <a href='#' className='px-1 text-white'>
+                Home
+              </a>
+              <a href='#' className='px-1 text-white'>
+                About
+              </a>
+              <a href='#' className='px-1 text-white'>
+                Portfolio
+              </a>
+              <a href='#' className='px-1 text-white'>
+                Resume
+              </a>
+              <a href='#' className='px-1 text-white'>
+                Hire Me 🙏
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Router } from 'react-router-dom';
+import { Navbar } from './components';
 import style from './global.css';
 
 const App = () => {
   return (
-    <div className=''>
-      {/* Header */}
-
-      {/* Body */}
-
-      {/* Footer */}
+    <div className='flex flex-col min-h-screen'>
+      {/* <header className='p-4 bg-blue-200'>Header</header> */}
+      <Navbar />
+      <div className='flex flex-row flex-1'>
+        <main className='flex-1 p-4 text-white bg-black'>Content</main>
+        {/* <nav className='order-first w-32 p-4 bg-blue-100'>Navigation</nav>
+        <aside className='w-32 p-4 bg-blue-100'>Side</aside> */}
+      </div>
+      <footer className='p-4 bg-blue-200'>Footer</footer>
     </div>
   );
 };
